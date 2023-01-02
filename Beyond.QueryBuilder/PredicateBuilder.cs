27222d73c6df46fa2bc77f8bc15c27f523b0caeb
hamedfathi <hamedfathi@outlook.com>
2023-01-02 14:50:15 +0100

    feat(PredicateBuilder): updated to new methods names.

--- Beyond.QueryBuilder/PredicateBuilder.cs
+++ Beyond.QueryBuilder/PredicateBuilder.cs
@@ -7,7 +7,7 @@ namespace Beyond.QueryBuilder;
 
 public static class PredicateBuilder
 {
-    public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1,
+    public static Expression<Func<T, bool>> AndWith<T>(this Expression<Func<T, bool>> expr1,
         Expression<Func<T, bool>> expr2)
     {
         var invokedExpr = Expression.Invoke(expr2, expr1.Parameters);
@@ -15,30 +15,30 @@ public static class PredicateBuilder
             (Expression.AndAlso(expr1.Body, invokedExpr), expr1.Parameters);
     }
 
-    public static Expression<Func<T, bool>> False<T>()
+    public static Expression<Func<T, bool>> FalseExpression<T>()
     { return _ => false; }
 
-    public static Expression<Func<T, bool>> Nand<T>(this Expression<Func<T, bool>> expr1,
+    public static Expression<Func<T, bool>> NandWith<T>(this Expression<Func<T, bool>> expr1,
         Expression<Func<T, bool>> expr2)
     {
-        var andExpr = expr1.And(expr2);
-        return andExpr.Not();
+        var andExpr = expr1.AndWith(expr2);
+        return andExpr.NotExpression();
     }
 
-    public static Expression<Func<T, bool>> Nor<T>(this Expression<Func<T, bool>> expr1,
+    public static Expression<Func<T, bool>> NorWith<T>(this Expression<Func<T, bool>> expr1,
         Expression<Func<T, bool>> expr2)
     {
-        var orExpr = expr1.Or(expr2);
-        return orExpr.Not();
+        var orExpr = expr1.OrWith(expr2);
+        return orExpr.NotExpression();
     }
 
-    public static Expression<Func<T, bool>> Not<T>(this Expression<Func<T, bool>> expr)
+    public static Expression<Func<T, bool>> NotExpression<T>(this Expression<Func<T, bool>> expr)
     {
         var notExpr = Expression.Not(expr.Body);
         return Expression.Lambda<Func<T, bool>>(notExpr, expr.Parameters);
     }
 
-    public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1,
+    public static Expression<Func<T, bool>> OrWith<T>(this Expression<Func<T, bool>> expr1,
         Expression<Func<T, bool>> expr2)
     {
         var invokedExpr = Expression.Invoke(expr2, expr1.Parameters);
@@ -46,17 +46,17 @@ public static class PredicateBuilder
             (Expression.OrElse(expr1.Body, invokedExpr), expr1.Parameters);
     }
 
-    public static Expression<Func<T, bool>> True<T>()
+    public static Expression<Func<T, bool>> TrueExpression<T>()
     { return _ => true; }
 
-    public static Expression<Func<T, bool>> Xnor<T>(this Expression<Func<T, bool>> expr1,
+    public static Expression<Func<T, bool>> XnorWith<T>(this Expression<Func<T, bool>> expr1,
         Expression<Func<T, bool>> expr2)
     {
-        var xorExpr = expr1.Xor(expr2);
-        return xorExpr.Not();
+        var xorExpr = expr1.XorWith(expr2);
+        return xorExpr.NotExpression();
     }
 
-    public static Expression<Func<T, bool>> Xor<T>(this Expression<Func<T, bool>> expr1,
+    public static Expression<Func<T, bool>> XorWith<T>(this Expression<Func<T, bool>> expr1,
         Expression<Func<T, bool>> expr2)
     {
         var invokedExpr = Expression.Invoke(expr2, expr1.Parameters);
